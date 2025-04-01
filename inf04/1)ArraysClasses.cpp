0da974f4b1d9e21a5640653c10eb3d61f665f69c
00chestnut <maksymilian@witoszek.com>
2025-04-01 11:24:06 +0200

    INF04 nr1 attepmt working

---
+++ inf04/1)ArraysClasses.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Tablica{
+const int size = 10;
+int array[i];
+
+    Tablica(&size)
+    {
+        cout << "parametrowy bazowej" << endl;
+    };
+};
+
+int main()
+{
+
+
+    return 00;
+}
+// https://arkusze.pl/zawodowy/inf04-2025-styczen-egzamin-zawodowy-praktyczny.pdf
