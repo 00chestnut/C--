71842682c9c3d45140388ae09179931ab66447de
00chestnut <maksymilian@witoszek.com>
2025-04-02 08:41:25 +0200

    d

--- inf04/1)ArraysClasses.cpp
+++ inf04/1)ArraysClasses.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
 using namespace std;
 
-class Tablica{
-const int size = 10;
-int array[i];
+class Tablica
+{
+    int SIZE;
+    int *dynArr = new int[SIZE];
 
-    Tablica(&size)
+public:
+    void setSize(int siize)
     {
-        cout << "parametrowy bazowej" << endl;
+        SIZE = siize;
+    }
+    int getSize()
+    {
+        return SIZE;
+    }
+    void fill(int SIZE)
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            dynArr[i] = rand() % 1001;
+        }
+    }
+    Tablica(int size)
+    {
+        SIZE = size;
+        dynArr = new int[SIZE];
+        fill(size);
     };
 };
 
 int main()
 {
 
-
+    Tablica tab;
     return 00;
 }
 // https://arkusze.pl/zawodowy/inf04-2025-styczen-egzamin-zawodowy-praktyczny.pdf
