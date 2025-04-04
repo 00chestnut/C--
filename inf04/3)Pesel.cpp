e6454f255d748cead53ec3e2ee801932f8224921
00chestnut <maksymilian@witoszek.com>
2025-04-04 19:22:18 +0200

    Finished pesel

--- inf04/3)Pesel.cpp
+++ inf04/3)Pesel.cpp
@@ -17,7 +17,14 @@ char plec(string PESEL)
 int control(string PESEL)
 {
 int S =   ((PESEL[0] - '0')*1)+((PESEL[1] - '0')*3)+((PESEL[2] - '0')*7)+((PESEL[3] - '0')*9)+((PESEL[4] - '0')*1)+((PESEL[5] - '0')*3)+((PESEL[6] - '0')*7)+((PESEL[7] - '0')*9)+((PESEL[8] - '0')*1)+((PESEL[9] - '0')*3);
-return S;
+int M = (S % 10);
+if(M == 0){
+int R = 0;
+return R;
+}else{
+int R = 10-M;
+return R;
+}
 }
 
 int main()
@@ -25,7 +32,12 @@ int main()
     string PESEL = "55030101193";
     cout << "\nPodaj PESEL: ";
     // cin >> PESEL;
-    cout << endl << "Fakt: " << plec(PESEL) << endl;
-    cout << endl << "Fakt: " << control(PESEL) << endl;
+    cout << endl << "Płeć: " << plec(PESEL) << endl;
+    cout << endl << "Liczba kontrolna: " << control(PESEL) << endl;
+    if((PESEL[11] - '0') == control(PESEL)){
+        cout << endl << "Pesel jest poprawny" << endl;
+    }else{
+        cout << "Pesel nie jest poprawny";
+    }
     return 0;
 }
