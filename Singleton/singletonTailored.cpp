f1d78fd1427880bf26bcb7dd563bc294a84a59e6
00chestnut <maksymilian@witoszek.com>
2025-04-14 10:15:47 +0200

    Singleton improved

--- Singleton/singletonTailored.cpp
+++ Singleton/singletonTailored.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 using namespace std;
 
-class Singleton {
+class Singleton
+{
 private:
-    static Singleton* instance;  
-    Singleton() {
-        cout << "I am the Singleton!" << endl;
-    }
+    static Singleton *instance;
+    Singleton() {}
 public:
-    static Singleton* getInstance() {
-        if (instance == nullptr) {
+    static Singleton *getInstance()
+    {
+        if (instance == nullptr)
+        {
             instance = new Singleton();
         }
-        return instance; 
+        return instance;
     }
-    void sayHi() {
-        cout << "Hello, I am Singleton!" << endl;
+    void sayHi()
+    {
+        cout << "I am Singleton" << endl;
     }
 };
-Singleton* Singleton::instance = nullptr;
-int main() {
-    Singleton* s = Singleton::getInstance(); 
-    s->sayHi();  
-    Singleton* t = Singleton::getInstance();  
-    t->sayHi();  
+Singleton *Singleton::instance = nullptr;
+int main()
+{
+    Singleton *s = Singleton::getInstance();
+    s->sayHi();
+    Singleton *t = Singleton::getInstance();
+    t->sayHi();
     return 0;
 }
